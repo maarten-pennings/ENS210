64f1373e0f3c6880be97f18737507c34b9bc3030
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2017-08-06 12:05:15 +0200

    Textual updates

--- src/ens210.h
+++ src/ens210.h
@@ -9,6 +9,7 @@
 #include <stdint.h>
 
 
+// Measurement status as output by `measure` and `extract`.
 #define ENS210_STATUS_OK        1
 #define ENS210_STATUS_INVALID   2
 #define ENS210_STATUS_CRCERROR  3
@@ -18,14 +19,14 @@
 class ENS210 {
   public: // Main API functions
     // Resets ENS210 and checks its PART_ID. Returns false on I2C problems or wrong PART_ID.
-    // Optionally pass a solder correction (units: 1/64K, default is 50mK).
+    // Optionally pass a solder `correction` (units: 1/64K, default is 50mK).
     // See "Effect of Soldering on Temperature Readout" in "Design-Guidelines"
     // https://download.ams.com/ENVIRONMENTAL-SENSORS/ENS210/Documentation
     bool begin(int correction=50*64/1000);                                
     // Performs one single shot temperature and relative humidity measurement. 
-    // Sets t_data (temperature in 1/64K), and t_status (from ENS210STATUS_XXX).
-    // Sets h_data (relative humidity in 1/512 %RH), and h_status (from ENS210STATUS_XXX).
-    // Use the conversion functions below to convert t_data to K, C, F; or h_data to %RH.
+    // Sets `t_data` (temperature in 1/64K), and `t_status` (from ENS210STATUS_XXX).
+    // Sets `h_data` (relative humidity in 1/512 %RH), and `h_status` (from ENS210STATUS_XXX).
+    // Use the conversion functions below to convert `t_data` to K, C, F; or `h_data` to %RH.
     // Note that this function contains a delay of 130ms to wait for the measurement to complete.
     void measure(int * t_data, int * t_status, int * h_data, int * h_status ); 
 
@@ -33,19 +34,21 @@ class ENS210 {
     int32_t toKelvin     (int t_data, int multiplier); // Converts t_data (from `measure`) to multiplier*Kelvin
     int32_t toCelsius    (int t_data, int multiplier); // Converts t_data (from `measure`) to multiplier*Celsius
     int32_t toFahrenheit (int t_data, int multiplier); // Converts t_data (from `measure`) to multiplier*Fahrenheit
-    int32_t toPercentageH(int h_data, int multiplier); // Converts t_data (from `measure`) to multiplier*%RH
+    int32_t toPercentageH(int h_data, int multiplier); // Converts h_data (from `measure`) to multiplier*%RH
 
-  public: // Helpers function
+  public: // Helper functions (communicating with ENS210)
     bool reset(void);                                  // Sends a reset to the ENS210. Returns false on I2C problems.
     bool lowpower(bool enable);                        // Sets ENS210 to low (true) or high (false) power. Returns false on I2C problems.
     bool getversion(uint16_t*partid,uint64_t*uid);     // Reads PART_ID and UID of ENS210. Returns false on I2C problems.
     bool startsingle(void);                            // Configures ENS210 to perform one single shot measurement. Returns false on I2C problems.
     bool read(uint32_t*t_val,uint32_t*h_val);          // Reads measurement data from the ENS210. Returns false on I2C problems.
+  
+  public: // Helper functions (data conversion)
     void extract(uint32_t val,int*data,int*status);    // Extracts measurement `data` and `status` from a `val` obtained from `read`.
     const char * status_str( int status );             // Converts a status (ENS210_STATUS_XXX) to a human readable string.
 
-  private:
-    int _soldercorrection;                             // Correction due to soldering (in 1/64K) subtracted from t_val by conversion functions.
+  private: // Data members
+    int _soldercorrection;                             // Correction due to soldering (in 1/64K); subtracted from `t_data` by conversion functions.
 };
 
 
